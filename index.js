fdef87769d7f3af7acaedbf8427a58f7ee0d8dc8
alfianalamsyahjk <alfianalamsyah9a@gmail.com>
2023-04-27 01:58:41 +0700

    ganti port jadi string

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ app.get("/json", (req, res) => {
     res.json({"Choo Choo": "Welcome to your Express app 🚅"});
 })
 
-const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen("3000", () => {
     console.log(`Example app listening on port ${port}`)
 })
 
